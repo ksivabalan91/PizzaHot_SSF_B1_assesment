994bcdd3ddd85b53af43c1e48b9991ba32dc5474
ksivabalan91 <ksivabalan.91@gmail.com>
2023-03-01 14:06:10 +0800

    initial commit, maven project initialized with relevant dependancies

---
+++ src/main/java/com/pizzahot/pizzahot/PizzahotApplication.java
@@ -0,0 +1,13 @@
+package com.pizzahot.pizzahot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzahotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzahotApplication.class, args);
+	}
+
+}
